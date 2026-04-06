e012e17470ec30ab87d3ec16be3f14642107293d
ellie-bellie <114118674+ellie-bellie@users.noreply.github.com>
2026-04-06 15:56:58 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mood-tracker-v4'; // Changed to v3 to force update
+const CACHE_NAME = 'mood-tracker-v5'; // Changed to v3 to force update
 const urlsToCache = [
   '/',
   '/index.html',
